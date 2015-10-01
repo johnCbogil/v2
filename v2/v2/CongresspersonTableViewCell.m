f8ce406ef76e502482f003dceb2ecafaee0f5364
John Bogil <johncbogil@gmail.com>
2015-10-01 10:37:48 -0400

    got basic call functionality working, need to make it dynamicnow

--- v2/v2/CongresspersonTableViewCell.m
+++ v2/v2/CongresspersonTableViewCell.m
@@ -7,6 +7,10 @@
 //
 
 #import "CongresspersonTableViewCell.h"
+@interface CongresspersonTableViewCell() <UIAlertViewDelegate>
+@property (weak, nonatomic) IBOutlet UIButton *callButton;
+
+@end
 
 @implementation CongresspersonTableViewCell
 
@@ -29,15 +33,42 @@
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
-
+    
     // Configure the view for the selected state
 }
+- (IBAction)callButtonDidPress:(id)sender {
+    
+    UIAlertView *confirmCallAlert = [[UIAlertView alloc]initWithTitle:@"Senator XYZ" message:@"You're about to call Senator XYZ, do you know what to say?" delegate:nil cancelButtonTitle:@"No" otherButtonTitles:@"Yes", nil];
+    [confirmCallAlert show];
+    confirmCallAlert.delegate = self;
+    
+}
+
+-(void) alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    
+    if (buttonIndex == 0) {
+        
+        NSLog(@"No");
+    }
+    else if (buttonIndex == 1) {
+        NSURL* callUrl=[NSURL URLWithString:[NSString   stringWithFormat:@"tel:5164589308"]];
+        if([[UIApplication sharedApplication] canOpenURL:callUrl])
+        {
+            [[UIApplication sharedApplication] openURL:callUrl];
+        }
+        else {
+            UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"ALERT" message:@"This function is only available on the iPhone"  delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alert show];
+        }
+    }
+}
+
 //
 //- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
 //    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
 //
 //
-//    
+//
 //    return self;
 //}
 @end
