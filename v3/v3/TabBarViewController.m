f384713a4a1844389bf9c70f8dd286c06d9ed32b
John Bogil <johncbogil@gmail.com>
2016-05-14 13:32:01 -0400

    Resolved storyboard architecture

--- v3/v3/TabBarViewController.m
+++ v3/v3/TabBarViewController.m
@@ -8,6 +8,8 @@
 
 #import "TabBarViewController.h"
 #import "UIColor+voicesOrange.h"
+#import "HomeViewController.h"
+#import "AdvocacyGroupsViewController.h"
 
 @interface TabBarViewController ()
 
@@ -17,14 +19,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    [UITabBar appearance].tintColor = [UIColor voicesOrange];
     
+    UIStoryboard *groupsSB = [UIStoryboard storyboardWithName:@"AdvocacyGroups" bundle: nil];
+    UIStoryboard *actionsSB = [UIStoryboard storyboardWithName:@"Actions" bundle:nil];
+
+    HomeViewController *homeVC = (HomeViewController *)[actionsSB instantiateViewControllerWithIdentifier: @"HomeViewController"];
+    AdvocacyGroupsViewController *groupsVC = (AdvocacyGroupsViewController *)[groupsSB instantiateViewControllerWithIdentifier: @"AdvocacyGroupsNavigationViewController"];
+    
+    self.viewControllers = @[homeVC, groupsVC];
+    
+    [[self.tabBar.items objectAtIndex:0] setImage:[UIImage imageNamed:@"Triangle"]];
+    [[self.tabBar.items objectAtIndex:1] setImage:[UIImage imageNamed:@"Circle"]];
+
     for(UITabBarItem * tabBarItem in self.tabBar.items){
         tabBarItem.title = @"";
         tabBarItem.imageInsets = UIEdgeInsetsMake(5, 0, -5, 0);
     }
-    
-    [UITabBar appearance].tintColor = [UIColor voicesOrange];
 }
 
 @end
