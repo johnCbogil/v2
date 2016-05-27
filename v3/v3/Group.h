e63ddf2b6340f106e811760296e07cb6bc1c34d1
Bogil, John <John.Bogil@viacommix.com>
2016-05-27 15:44:08 -0400

    Following groups is not working yet, will need Group models to work properly I believe

---
+++ v3/v3/Group.h
@@ -0,0 +1,13 @@
+//
+//  Group.h
+//  Voices
+//
+//  Created by Bogil, John on 5/27/16.
+//  Copyright © 2016 John Bogil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Group : NSObject
+
+@end
---
+++ v3/v3/Group.m
@@ -0,0 +1,16 @@
+//
+//  Group.m
+//  Voices
+//
+//  Created by Bogil, John on 5/27/16.
+//  Copyright © 2016 John Bogil. All rights reserved.
+//
+
+#import "Group.h"
+
+@implementation Group
+
+- (void)initWithData:(NSDictionary *)data {
+    
+}
+@end
