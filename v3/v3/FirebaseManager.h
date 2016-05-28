d61281884c0f746e1fc0d91628d23f2aaea98809
David Weissler <davidweissler@Davids-MBP.fios-router.home>
2016-05-28 17:08:53 -0400

    FirebaseManager initial commit

---
+++ v3/v3/FirebaseManager.h
@@ -0,0 +1,13 @@
+//
+//  FirebaseManager.h
+//  Voices
+//
+//  Created by David Weissler on 5/28/16.
+//  Copyright © 2016 John Bogil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FirebaseManager : NSObject
+
+@end
