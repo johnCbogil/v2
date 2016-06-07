57387963cacc52b00a85b2f37414952934851638
David Weissler <davidweissler@Davids-MBP.fios-router.home>
2016-06-06 22:21:24 -0400

    Action initial commit

---
+++ v3/v3/Action.h
@@ -0,0 +1,13 @@
+//
+//  Action.h
+//  Voices
+//
+//  Created by David Weissler on 6/6/16.
+//  Copyright © 2016 John Bogil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Action : NSObject
+
+@end
---
+++ v3/v3/Action.m
@@ -0,0 +1,13 @@
+//
+//  Action.m
+//  Voices
+//
+//  Created by David Weissler on 6/6/16.
+//  Copyright © 2016 John Bogil. All rights reserved.
+//
+
+#import "Action.h"
+
+@implementation Action
+
+@end
