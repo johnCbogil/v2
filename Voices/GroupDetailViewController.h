1b727cb752a1d0f5f65492fae097415713ddf4cd
John Bogil <johncbogil@gmail.com>
2016-07-05 21:54:11 -0400

    Initial commit, created viewcontroller

---
+++ Voices/GroupDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  GroupDetailViewController.h
+//  Voices
+//
+//  Created by John Bogil on 7/5/16.
+//  Copyright © 2016 John Bogil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GroupDetailViewController : UIViewController
+
+@end
---
+++ Voices/GroupDetailViewController.m
@@ -0,0 +1,23 @@
+//
+//  GroupDetailViewController.m
+//  Voices
+//
+//  Created by John Bogil on 7/5/16.
+//  Copyright © 2016 John Bogil. All rights reserved.
+//
+
+#import "GroupDetailViewController.h"
+
+@interface GroupDetailViewController ()
+
+@end
+
+@implementation GroupDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
