707aaf0dcfcf65b01c7f4fabd92e930a5d7fea0a
John Bogil <johncbogil@gmail.com>
2016-07-14 23:02:30 -0400

    Created a view controller for the notification auth

---
+++ Voices/NotiOnboardingViewController.m
@@ -0,0 +1,37 @@
+//
+//  NotiOnboardingViewController.m
+//  Voices
+//
+//  Created by John Bogil on 7/14/16.
+//  Copyright © 2016 John Bogil. All rights reserved.
+//
+
+#import "NotiOnboardingViewController.h"
+
+@interface NotiOnboardingViewController ()
+
+@end
+
+@implementation NotiOnboardingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
