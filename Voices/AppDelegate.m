eba613f724f55938a2dabad13bb5abfe5c5edb85
John Bogil <johncbogil@gmail.com>
2016-07-16 14:38:48 -0400

    onbaroding bug fix

--- Voices/AppDelegate.m
+++ Voices/AppDelegate.m
@@ -25,7 +25,6 @@
 
 @interface AppDelegate ()
 
-
 @end
 
 @implementation AppDelegate
--- Voices/FederalRepresentativeTableViewCell.m
+++ Voices/FederalRepresentativeTableViewCell.m
@@ -14,8 +14,6 @@
 #import <MessageUI/MFMailComposeViewController.h>
 #import "VoicesConstants.h"
 
-@import FirebaseAnalytics;
-
 @interface FederalRepresentativeTableViewCell() <UIAlertViewDelegate, MFMailComposeViewControllerDelegate>
 
 @property (strong, nonatomic) FederalRepresentative *federalRepresentative;
@@ -85,8 +83,6 @@
             
             [[UIApplication sharedApplication] openURL:callUrl];
             
-            [FIRAnalytics logEventWithName:@"phone_call"
-                                parameters:@{ @"name": self.federalRepresentative.fullName }];
         }
         else {
             UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"Oops" message:@"This representative hasn't given us their phone number"  delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
