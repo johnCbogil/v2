a1abc7bfb976b8e0fb687e0726fc95b43254f277
John Bogil <johncbogil@gmail.com>
2016-07-16 12:27:31 -0400

    Added a next button to the noti onboarding

--- Voices/NotiOnboardingViewController.m
+++ Voices/NotiOnboardingViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "NotiOnboardingViewController.h"
+#import "LocationOnboardingViewController.h"
 #import "VoicesConstants.h"
 #import "UIColor+voicesColor.h"
 #import "UIFont+voicesFont.h"
@@ -21,7 +22,7 @@
 @property (weak, nonatomic) IBOutlet UIImageView *notiOnboardingImageView;
 @property (weak, nonatomic) IBOutlet UIView *singleLineView;
 @property (weak, nonatomic) IBOutlet UIButton *turnOnNotificationsButton;
-@property (weak, nonatomic) IBOutlet UIButton *deferNotiButton;
+@property (weak, nonatomic) IBOutlet UIButton *nextButton;
 
 @end
 
@@ -29,17 +30,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    
-    AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate]   ;
-    
 
+    self.nextButton.enabled = NO;
     
+    [self setFont];
+}
+
+- (void)setFont {
     
-    [appDelegate addObserver:self forKeyPath:@"isRegisteredForPushNotis" options:NSKeyValueObservingOptionNew context:nil];
-
-//    [[LocationService sharedInstance] addObserver:self forKeyPath:@"currentLocation" options:NSKeyValueObservingOptionNew context:nil];
-
+    self.turnOnNotificationsButton.titleLabel.font = [UIFont voicesFontWithSize:21];
+    self.turnOnNotificationsButton.backgroundColor = [UIColor voicesOrange];
+    self.turnOnNotificationsButton.layer.cornerRadius = kButtonCornerRadius;
+    
+    self.nextButton.titleLabel.font = [UIFont voicesFontWithSize:23];
+    [self.nextButton setTitleColor:[UIColor voicesGray] forState:UIControlStateNormal];
+    self.nextButton.alpha = 0.5;
 }
 
 - (IBAction)turnOnNotiButtonDidPress:(id)sender {
@@ -48,14 +53,17 @@
     UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
     [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
     [[UIApplication sharedApplication] registerForRemoteNotifications];
+    
+    self.nextButton.enabled = YES;
+    [self.nextButton setTitleColor:[UIColor voicesOrange] forState:UIControlStateNormal];
+    self.nextButton.alpha = 1.0;
 }
 
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
-    if ([keyPath isEqualToString:@"isRegisteredForPushNotis"]) {
-        NSLog(@"OtherVC: The value of self.myVC.mySetting has changed");
-    }
+- (IBAction)nextButtonDidPress:(id)sender {
+    
+    UIStoryboard *onboardingStoryboard = [UIStoryboard storyboardWithName:@"Onboarding" bundle: nil];
+    LocationOnboardingViewController *locationOnboardingViewController = (LocationOnboardingViewController *)[onboardingStoryboard instantiateViewControllerWithIdentifier: @"LocationOnboardingViewController"];
+    [self.navigationController pushViewController:locationOnboardingViewController animated:YES];
 }
-
-
 
 @end
