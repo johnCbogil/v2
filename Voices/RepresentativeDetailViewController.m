b5334877c4038ec20749362c31061fed93a50033
Bogil, John <John.Bogil@viacommix.com>
2016-08-25 17:34:15 -0400

    Background needed a color to prevent choppiness

--- Voices/RepresentativeDetailViewController.m
+++ Voices/RepresentativeDetailViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self.navigationController setNavigationBarHidden:NO];
+    self.view.backgroundColor = [UIColor redColor];
     
 }
 
