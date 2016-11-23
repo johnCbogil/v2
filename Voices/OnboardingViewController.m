ae179f33dda0b03ea021f69176d3c98e17c43b88
John Bogil <johncbogil@gmail.com>
2016-11-22 20:40:44 -0500

    Updated onboarding text

--- Voices/OnboardingViewController.m
+++ Voices/OnboardingViewController.m
@@ -34,7 +34,7 @@
     self.startButton.titleLabel.minimumScaleFactor = 0.75;
 
     self.introLabel.font = [UIFont voicesFontWithSize:25.0];
-    self.introLabel.text = @"Voices is an advocacy tool for the people who care.";
+    self.introLabel.text = @"Voices helps you take action for the causes you care about.";
 }
 
 @end
