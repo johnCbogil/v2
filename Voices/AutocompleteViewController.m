f676211161ec30933052515688f4a6f303f7638e
John Bogil <johncbogil@gmail.com>
2017-02-16 22:47:10 -0500

    Created new vc

---
+++ Voices/AutocompleteViewController.m
@@ -0,0 +1,22 @@
+//
+//  AutocompleteViewController.m
+//  Voices
+//
+//  Created by John Bogil on 2/16/17.
+//  Copyright © 2017 John Bogil. All rights reserved.
+//
+
+#import "AutocompleteViewController.h"
+
+@interface AutocompleteViewController ()
+
+@end
+
+@implementation AutocompleteViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
