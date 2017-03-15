204d36939528625deb42e8bf27e8deef054d1e68
Andy Wu <andywu@Andys-Mac.local>
2017-03-15 16:58:13 -0400

    Created More folder, storyboard and VC

---
+++ Voices/MoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoreViewController.h
+//  Voices
+//
+//  Created by Andy Wu on 3/15/17.
+//  Copyright © 2017 John Bogil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreViewController : UIViewController
+
+@end
