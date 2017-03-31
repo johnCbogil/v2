58a341b1f2a0e6c9c080bb65034f279196c1a457
John Bogil <johncbogil@gmail.com>
2017-03-31 11:12:53 -0400

    Added a nil check for state phones

--- Voices/RepTableViewCell.m
+++ Voices/RepTableViewCell.m
@@ -89,7 +89,8 @@
 #pragma mark - User Actions
 
 - (IBAction)callButtonDidPress:(id)sender {
-    if (self.representative.phone.length) {
+    
+    if (self.representative.phone.length > 0) {
         NSString *confirmCallMessage;
         if (self.representative.nickname != nil && ![self.representative.nickname isEqual:[NSNull null]]) {
             confirmCallMessage =  [NSString stringWithFormat:@"You're about to call %@, do you know what to say?", self.representative.nickname];
