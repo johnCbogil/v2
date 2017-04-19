ae1c3d9af631717c4418d44e484cbb337a1bc39c
Andy Wu <andywu@Andys-Mac.local>
2017-04-19 12:16:53 -0400

    Removed duplicate code in FirebaseManager

--- Voices/FirebaseManager.m
+++ Voices/FirebaseManager.m
@@ -309,26 +309,23 @@
             NSLog(@"%@", snapshot.value);
             Action *newAction = [[Action alloc] initWithKey:actionKey actionDictionary:snapshot.value];
             
-            NSDate *currentTime = [NSDate date];
+            // TODO: REPEAT CODE HERE AND BELOW
+            NSNumber *timestamp = [NSNumber numberWithDouble:newAction.timestamp];
+            BOOL debug = [self checkTimeStamp:[timestamp stringValue]];
             
-            if(newAction.timestamp < currentTime.timeIntervalSince1970 - (3600 * 4)) {
-                
-                // TODO: REPEAT CODE HERE AND BELOW
-                BOOL debug = [self isInDebugMode];
-                // if app is in debug, add all groups
-                if (debug) {
-                    [[CurrentUser sharedInstance].listOfActions addObject:newAction];
-                }
-                // if app is not in debug, add only non-debug groups
-                else if (!newAction.debug) {
-                    [[CurrentUser sharedInstance].listOfActions addObject:newAction];
-                }
-                
-                NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"timestamp" ascending:NO];
-                NSArray *sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
-                [CurrentUser sharedInstance].listOfActions = [[CurrentUser sharedInstance].listOfActions sortedArrayUsingDescriptors:sortDescriptors].mutableCopy;
-                successBlock([CurrentUser sharedInstance].listOfActions);
+            // if app is in debug, add all groups
+            if (debug) {
+                [[CurrentUser sharedInstance].listOfActions addObject:newAction];
             }
+            // if app is not in debug, add only non-debug groups
+            else if (!newAction.debug) {
+                [[CurrentUser sharedInstance].listOfActions addObject:newAction];
+            }
+            
+            NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"timestamp" ascending:NO];
+            NSArray *sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
+            [CurrentUser sharedInstance].listOfActions = [[CurrentUser sharedInstance].listOfActions sortedArrayUsingDescriptors:sortDescriptors].mutableCopy;
+            successBlock([CurrentUser sharedInstance].listOfActions);
         }];
     }
     successBlock([CurrentUser sharedInstance].listOfActions);
@@ -348,28 +345,24 @@
             }
             Action *action = [[Action alloc] initWithKey:actionKey actionDictionary:snapshot.value];
             
-            NSDate *currentTime = [NSDate date];
-            
             // TODO: REPEAT CODE HERE AND ABOVE
-            if (action.timestamp < currentTime.timeIntervalSince1970  - (3600 * 4)) {
-                
-                BOOL debug = [self isInDebugMode];
-                // if app is in debug, add all groups
-                if (debug) {
-                    [[CurrentUser sharedInstance].listOfActions addObject:action];
-                }
-                // if app is not in debug, add only non-debug groups
-                else if (!action.debug) {
-                    [[CurrentUser sharedInstance].listOfActions addObject:action];
-                }
-                
-                NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"timestamp" ascending:NO];
-                NSArray *sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
-                [CurrentUser sharedInstance].listOfActions = [[CurrentUser sharedInstance].listOfActions sortedArrayUsingDescriptors:sortDescriptors].mutableCopy;
-                successBlock([CurrentUser sharedInstance].listOfActions);
-                [actions addObject:action];
-
+            NSNumber *timestamp = [NSNumber numberWithDouble:action.timestamp];
+            BOOL debug = [self checkTimeStamp:[timestamp stringValue]];
+            
+            // if app is in debug, add all groups
+            if (debug) {
+                [[CurrentUser sharedInstance].listOfActions addObject:action];
             }
+            // if app is not in debug, add only non-debug groups
+            else if (!action.debug) {
+                [[CurrentUser sharedInstance].listOfActions addObject:action];
+            }
+            
+            NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"timestamp" ascending:NO];
+            NSArray *sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
+            [CurrentUser sharedInstance].listOfActions = [[CurrentUser sharedInstance].listOfActions sortedArrayUsingDescriptors:sortDescriptors].mutableCopy;
+            successBlock([CurrentUser sharedInstance].listOfActions);
+            [actions addObject:action];
             dispatch_group_leave(actionsGroup);
         }];
     }
@@ -398,6 +391,17 @@
             errorBlock(error);
         }];
     }
+}
+
+#pragma mark - Check Timestamp
+
+- (BOOL)checkTimeStamp:(NSString *)timeStamp {
+    NSDate *currentTime = [NSDate date];
+    BOOL debug = false;
+    if([timeStamp doubleValue] < currentTime.timeIntervalSince1970 - (3600 * 4)) {
+        debug = [self isInDebugMode];
+    }
+    return debug;
 }
 
 #pragma mark - Private methods
