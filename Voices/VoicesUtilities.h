9f9ca8c915fb2730272d09b308820178022969ef
John Bogil <johncbogil@gmail.com>
2017-05-11 09:18:36 -0400

    Started

---
+++ Voices/VoicesUtilities.h
@@ -0,0 +1,15 @@
+//
+//  VoicesUtilities.h
+//  Voices
+//
+//  Created by John Bogil on 5/11/17.
+//  Copyright © 2017 John Bogil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VoicesUtilities : NSObject
+
++ (NSString *)getEmojis;
+
+@end
