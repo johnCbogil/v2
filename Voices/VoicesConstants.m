91e6044cfa4351eeeebb7f25153e9440f77a2dbe
Bogil, John <John.Bogil@viacommix.com>
2017-06-05 20:56:39 -0400

    Updated api key for openstates

--- Voices/VoicesConstants.m
+++ Voices/VoicesConstants.m
@@ -25,7 +25,7 @@ NSString *const kAvenirNextBold = @"AvenirNext-Bold";
 //NSString *const kOpenSans = @"OpenSans";
 
 NSString *const kSFCongress = @"a0c99640cc894383975eb73b99f39d2f";
-NSString *const kSFState = @"a0c99640cc894383975eb73b99f39d2f";
+NSString *const kSFState = @"cf138133-cf8c-4013-95c7-31d197203e02";
 NSString *const kGoogMaps = @"AIzaSyBr8fizIgU0OF53heFICd3ak5Yp1EJpviE";
 NSString *const kAutocomplete = @"AIzaSyBudmB9IiHLj9Bjw5BYdPikwNyVc0bTYHw";
 
