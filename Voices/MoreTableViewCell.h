f58edae06e9a3d14f1c94e74de703ee8c3e929cb
John Bogil <johncbogil@gmail.com>
2017-09-30 18:14:06 -0400

    k

---
+++ Voices/MoreTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  MoreTableViewCell.h
+//  Voices
+//
+//  Created by John Bogil on 9/30/17.
+//  Copyright © 2017 John Bogil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *emojiLabel;
+
+@end
---
+++ Voices/MoreTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MoreTableViewCell.m
+//  Voices
+//
+//  Created by John Bogil on 9/30/17.
+//  Copyright © 2017 John Bogil. All rights reserved.
+//
+
+#import "MoreTableViewCell.h"
+
+@implementation MoreTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
