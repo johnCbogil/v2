a4f47f922c15f36af8a30e1cb6a23c8b90fdacb5
John Bogil <johncbogil@gmail.com>
2017-10-04 22:16:45 -0400

    Started group onbaording vc

---
+++ Voices/GroupOnboardingViewController.m
@@ -0,0 +1,31 @@
+//
+//  GroupOnboardingViewController.m
+//  Voices
+//
+//  Created by John Bogil on 10/4/17.
+//  Copyright © 2017 John Bogil. All rights reserved.
+//
+
+#import "GroupOnboardingViewController.h"
+
+@interface GroupOnboardingViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@end
+
+@implementation GroupOnboardingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    id cell;
+    return cell;
+}
+
+@end
