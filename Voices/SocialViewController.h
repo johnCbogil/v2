599944cfef262710ea5da2ed90bc27dccece4182
John Bogil <johncbogil@gmail.com>
2017-10-09 22:22:33 -0400

    Created the social storyboard and viewcontroller

---
+++ Voices/SocialViewController.h
@@ -0,0 +1,13 @@
+//
+//  SocialViewController.h
+//  Voices
+//
+//  Created by John Bogil on 10/9/17.
+//  Copyright © 2017 John Bogil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SocialViewController : UIViewController
+
+@end
---
+++ Voices/SocialViewController.m
@@ -0,0 +1,54 @@
+//
+//  SocialViewController.m
+//  Voices
+//
+//  Created by John Bogil on 10/9/17.
+//  Copyright © 2017 John Bogil. All rights reserved.
+//
+
+#import "SocialViewController.h"
+#import "FirebaseManager.h"
+#import "CurrentUser.h"
+
+@interface SocialViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation SocialViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    [self configureTableView];
+    
+//    // FOR EACH GROUP IN THE LIST OF USERS FOLLOWED GROUPS
+//    
+//    // FETCH ACTIONS
+//    [FirebaseManager sharedInstance]fetchActionsForGroup:<#(Group *)#> withCompletion:<#^(NSArray *listOfActions)successBlock#>
+    
+    // FOR EACH ACTION IN THE LIST OF ACTIOJNS
+    
+    // CREATE A USER COMPLETED MODEL OBJECT
+}
+
+- (void)configureTableView {
+    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    id cell;
+    return cell;
+}
+
+
+@end
